e5ea9fa02ed8c2ec91977fc03149636f8d199beb
Thiencc03-07-2006 <cct20062250.@gmail.com>
2026-04-07 08:02:57 +0700

    up

--- src/main/java/com/example/hn_ks24_cntt2_caochithien/controller/EmployeeController.java
+++ src/main/java/com/example/hn_ks24_cntt2_caochithien/controller/EmployeeController.java
@@ -1,14 +1,12 @@
 package com.example.hn_ks24_cntt2_caochithien.controller;
 
 import com.example.hn_ks24_cntt2_caochithien.model.Employee;
-import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-
-import org.springframework.stereotype.Controller;
 
 @Controller
 @RequestMapping("/employees")
@@ -17,9 +15,9 @@ public class EmployeeController {
     @GetMapping
     public String list(Model model) {
         model.addAttribute("employees", List.of(
-                new Employee(1, "Nguyen Van A", "nhan su", 10000000D),
-                new Employee(2, "Nguyen Van B", "nhan su", 20000000D),
-                new Employee(3, "Nguyen Van C", "nhan su", 30000000D)
+                new Employee(1, "Nguyen Van A", "Nhan su", 10000000D),
+                new Employee(2, "Nguyen Van B", "Nhan su", 20000000D),
+                new Employee(3, "Nguyen Van C", "Nhan su", 30000000D)
         ));
         return "employee-list";
     }
